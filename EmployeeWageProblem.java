f9a10828855a301ba34918824171da9dc86a4cad
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-07-25 01:49:11 +0530

    Added: UC5

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -6,21 +6,25 @@ public class EmployeeWageProblem {
 		final int partTime = 1;
 		final int fullTime = 2;
 		final int wagePerHour = 20;
-		int empType = (int) (Math.random() * 100) % 3;
-		int workingHours = 0;
-		switch (empType) {
-		case fullTime:
-			System.out.println("Employee is Present Full time.");
-			workingHours = 8;
-			break;
-		case partTime:
-			System.out.println("Employee is Present Part time.");
-			workingHours = 4;
-			break;
-		default:
-			System.out.println("Employee is Absent.");
+		final int workingDays = 20;
+
+		int totalWage = 0;
+		for (int day = 1; day <= workingDays; day++) {
+			int empType = (int) (Math.random() * 100) % 3;
+			int workingHours = 0;
+			switch (empType) {
+			case fullTime:
+				workingHours = 8;
+				break;
+			case partTime:
+				workingHours = 4;
+				break;
+			default:
+			}
+			int wage = workingHours * wagePerHour;
+			System.out.println("Day " + day + " wage is: " + wage);
+			totalWage += wage;
 		}
-		int wage = workingHours * wagePerHour;
-		System.out.println("Employee Daily Wage is " + wage + ".");
+		System.out.println("Total wage for the month is " + totalWage + ".");
 	}
 }
