8c57967f339727d5ccca55845e51ae2a9336abaa
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-07-25 01:32:47 +0530

    Added: UC2

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -3,17 +3,19 @@ package employeewageproblem;
 public class EmployeeWageProblem {
 
 	public static void main(String[] args) {
-		{
-			final int FULL_TIME = 1;
-			int empType = (int) (Math.random() * 100) % 2;
-			if (empType == FULL_TIME) {
-				System.out.println("Employee is Present");
 
-			} else {
-				System.out.println("Employee is Absent");
-
-			}
+		final int fullTime = 1;
+		final int wagePerHour = 20;
+		int empType = (int) (Math.random() * 100) % 2;
+		int workingHours = 0;
+		if (empType == fullTime) {
+			System.out.println("Employee is Present");
+			workingHours = 8;
+		} else {
+			System.out.println("Employee is Absent");
 		}
+		int wage = workingHours * wagePerHour;
+		System.out.println("Employee Daily Wage is " + wage);
 	}
 
 }
