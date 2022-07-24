9d2c311439488377bc79f8da143eb756dbceb45d
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-07-25 01:24:54 +0530

    Added: Welcome Message

---
+++ EmployeeWageProblem.java
@@ -0,0 +1,9 @@
+package employeewageproblem;
+
+public class EmployeeWageProblem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee wage computation program!");
+	}
+
+}
