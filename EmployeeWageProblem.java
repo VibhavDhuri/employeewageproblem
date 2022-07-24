b4a50ec75a953295bdd040dfb690a070fdd5283b
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-07-25 01:59:20 +0530

    Added: UC6

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -6,25 +6,32 @@ public class EmployeeWageProblem {
 		final int partTime = 1;
 		final int fullTime = 2;
 		final int wagePerHour = 20;
-		final int workingDays = 20;
+		final int maxWorkingDays = 20;
+		final int maxWorkingHours = 100;
 
 		int totalWage = 0;
-		for (int day = 1; day <= workingDays; day++) {
+		int workingHrs = 0;
+		System.out.printf("%5s %10s %6s %20s\n", "Day", "Working Hours", "Wage", "Total Working Hours");
+		for (int day = 1, totalWorkingHrs = 0; day <= maxWorkingDays
+				&& totalWorkingHrs < maxWorkingHours; day++, totalWorkingHrs += workingHrs) {
+
 			int empType = (int) (Math.random() * 100) % 3;
-			int workingHours = 0;
 			switch (empType) {
 			case fullTime:
-				workingHours = 8;
+				workingHrs = 8;
 				break;
 			case partTime:
-				workingHours = 4;
+				workingHrs = 4;
 				break;
 			default:
+				workingHrs = 0;
+				break;
 			}
-			int wage = workingHours * wagePerHour;
-			System.out.println("Day " + day + " wage is: " + wage);
+			int wage = workingHrs * wagePerHour;
 			totalWage += wage;
+			System.out.printf("%4d %7d %12d %8d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+
 		}
-		System.out.println("Total wage for the month is " + totalWage + ".");
+		System.out.println("Total wage for a month is " + totalWage + ".");
 	}
 }
